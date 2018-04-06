2c18f0075f5d2417ff61fe5894d38b15a66e6d7f
Jonathan Galentine <j.galentine@gmail.com>
2018-04-06 00:44:35 -0600

    Login form now working, added basic debug output of shared state.

--- src/main/java/com/intensityanalytics/openam/auth/nodes/LoginFormNode.java
+++ src/main/java/com/intensityanalytics/openam/auth/nodes/LoginFormNode.java
@@ -9,20 +9,21 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javax.inject.Inject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.NameCallback;
 import javax.security.auth.callback.PasswordCallback;
 
+import com.google.inject.assistedinject.Assisted;
 import com.sun.identity.shared.debug.Debug;
 import com.sun.identity.authentication.callbacks.HiddenValueCallback;
 import com.sun.identity.authentication.callbacks.ScriptTextOutputCallback;
 
 import org.forgerock.guava.common.base.Strings;
 import org.forgerock.json.JsonValue;
-import org.forgerock.openam.auth.node.api.Action;
-import org.forgerock.openam.auth.node.api.Node;
-import org.forgerock.openam.auth.node.api.SingleOutcomeNode;
-import org.forgerock.openam.auth.node.api.TreeContext;
+import org.forgerock.openam.annotations.sm.Attribute;
+import org.forgerock.openam.auth.node.api.*;
+import org.forgerock.openam.core.CoreWrapper;
 
 /**
  * A node which collects a username from the user via a name callback.
@@ -33,14 +34,33 @@ import org.forgerock.openam.auth.node.api.TreeContext;
         configClass = LoginFormNode.Config.class)
 public class LoginFormNode extends SingleOutcomeNode
 {
+    private final Config config;
+    private final CoreWrapper coreWrapper;
+    private static final String BUNDLE = "com/intensityanalytics/openam/auth/nodes/LoginFormNode";
     private final static String DEBUG_FILE = "LoginFormNode";
     protected Debug debug = Debug.getInstance(DEBUG_FILE);
 
     interface Config
     {
+        @Attribute(order = 100)
+        default String library()
+        {
+            return "//keyidservices.tickstream.com/library/keyid-min";
+        }
     }
 
-    private static final String BUNDLE = "com/intensityanalytics/openam/auth/nodes/LoginFormNode";
+    /**
+     * Create the node.
+     *
+     * @param config The service config.
+     * @throws NodeProcessException If the configuration was not valid.
+     */
+    @Inject
+    public LoginFormNode(@Assisted LoginFormNode.Config config, CoreWrapper coreWrapper) throws NodeProcessException
+    {
+        this.config = config;
+        this.coreWrapper = coreWrapper;
+    }
 
     @Override
     public Action process(TreeContext context)
@@ -67,6 +87,9 @@ public class LoginFormNode extends SingleOutcomeNode
             sharedState.get(USERNAME).isNotNull() &&
             sharedState.get(TSDATA).isNotNull())
         {
+            debug.message("username: " + sharedState.get(USERNAME));
+            debug.message("password: " + sharedState.get(PASSWORD));
+            debug.message("tsData: " + sharedState.get(TSDATA));
             return goToNext().replaceSharedState(sharedState).build();
         }
         else
@@ -82,9 +105,19 @@ public class LoginFormNode extends SingleOutcomeNode
         List<Callback> callBackList = new ArrayList<>();
         callBackList.add(new NameCallback(bundle.getString("callback.username")));
         callBackList.add(new PasswordCallback(bundle.getString("callback.password"), false));
-        callBackList.add(new ScriptTextOutputCallback("console.log('hello world');"));
+        callBackList.add(new ScriptTextOutputCallback(String.format(KEYIDSCRIPT, config.library())));
         callBackList.add(new HiddenValueCallback(TSDATA));
 
         return send(callBackList).build();
     }
+
+    private static final String KEYIDSCRIPT = "var script = document.createElement('script');\n" +
+                                              "script.onload = function () {\n" +
+                                              "    $('[name=\"callback_1\"]').attr('id', 'idToken1');\n" +
+                                              "    tsBindControl('idToken1', bandType.KeyID, true);\n" +
+                                              "    $('#loginButton_0').click(function(){populateControlWithKeyDataConcatenated('tsData');});\n" +
+                                              "};\n" +
+                                              "script.src = '%s'\n" +
+                                              "\n" +
+                                              "document.body.appendChild(script);";
 }
