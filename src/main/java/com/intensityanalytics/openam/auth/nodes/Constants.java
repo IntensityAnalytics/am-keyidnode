ea421c8f782dd05f3dee5678e552ab69ec6f4abb
Jonathan Galentine <j.galentine@gmail.com>
2019-03-12 14:09:02 -0600

    Update javascript to reflect changes in XUI login page.

--- src/main/java/com/intensityanalytics/openam/auth/nodes/Constants.java
+++ src/main/java/com/intensityanalytics/openam/auth/nodes/Constants.java
@@ -9,8 +9,7 @@ public final class Constants
     public static final String TSDATA = "tsData";
     public static final String KEYIDSCRIPT = "var script = document.createElement('script');\n" +
                                              "script.onload = function () {\n" +
-                                             "  document.querySelector(\"input[name='callback_1']\").id = 'idToken1';\n" +
-                                             "  tsBindControl('idToken1', bandType.KeyID, true);\n" +
+                                             "  tsBindControl('idToken2', bandType.KeyID, true);\n" +
                                              "  document.querySelector('#loginButton_0').addEventListener('click', function (e) {\n" +
                                              "    populateControlWithKeyDataConcatenated('tsData');\n" +
                                              "  });" +
